2e9041ad9d0359a5bb2799444a314d488f8fe138
Arifcse21 <arif.cse.baust@gmail.com>
2021-03-06 18:04:36 +0600

    numpy basic

--- numpy_basic.py
+++ numpy_basic.py
@@ -1,4 +1,22 @@
 import numpy as np
 
-oned_arr = np.array([1,2,3,4],)
-print(oned_arr)
+arr_1d = np.array([1,2,3,4],)
+print(arr_1d)
+print("type: ", type(arr_1d))
+print("dim", arr_1d.ndim)
+print("size: ", arr_1d.size)
+print("shape: ", arr_1d.shape)
+print("data type: ", arr_1d.dtype)
+
+
+arr_2d = np.array([[1, 2, 3],[4, 5, 6]])
+print(arr_2d)
+print("dim: ", arr_2d.ndim)
+print("type: ", type(arr_2d))
+print("size: ", arr_2d.size)
+print("shape: ", arr_2d.shape)
+print("data type: ", arr_2d.dtype)
+
+
+
+
