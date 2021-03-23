77cc9f5433786d3d1595487f8fbea727c27e41b2
Arifcse21 <arif.cse.baust@gmail.com>
2021-03-23 16:25:32 +0600

    random numpy

--- numpy_random.py
+++ numpy_random.py
@@ -15,5 +15,29 @@ print()
 for x in range(3):
     print(random.randint(0, 5))
 
-print(np.random.randint(1, 5, (3, 3)))
+print(np.random.randint(1, 5, (3, 3)))  # values (1-4) IN 3X3 array
 # print(random.randint(1, 5, (4, 4)))
+# ###################################################################
+# As everytime the random.randint() is different. We need to seed a label
+np.random.seed(22)      # seed value can be 2**64 -1
+print(np.random.randint(1, 4, (4, 4)))
+
+
+np.random.seed(22)      # seed value can be 2**64 -1
+print(np.random.randint(1, 4, (4, 4)))
+
+# ####################################################################
+
+print(np.random.rand(3, 3))     # random 3x3 array
+print(np.random.randn(4, 4))    # random(pos+neg) 3x3 array
+
+# #######CHOICE#######
+
+x = [1, 2, 5, 7]
+print(np.random.choice(x))
+
+# ###########Permutation############
+print(np.random.permutation(x))
+# print(np.random.shuffle(x))
+
+
